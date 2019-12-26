3e1acfa76944a864a85c0ee02dcbc3061c4c781d
mahmoud <ma7.aelaziz@gmail.com>
2019-12-27 00:57:00 +0200

    added set ratio feature

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ def index():
 
     if request.method == 'POST':
         volume = int(request.form['volume'])
-        epoxy = ((volume * 100) / 140)
-        hardner = volume - epoxy 
-        return render_template('index.html', volume=round(volume), epoxy=round(epoxy), hardner=round(hardner))
+        ratio_a = int(request.form['ratio_a'])
+        ratio_b = int(request.form['ratio_b'])
+        ## epoxy == (volume * ratio a)/(ratioa + ratio b)
+        epoxy = ((volume * ratio_a) / (ratio_a + ratio_b))
+        hardner = volume - epoxy
+
+        return render_template('index.html', volume=round(volume), epoxy=round(epoxy), hardner=round(hardner), ratio_a=round(ratio_a), ratio_b= round(ratio_b))
 
     else:
         return render_template('index.html')
